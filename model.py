79518c041bd9754a07f66ff47a079f89f2dca09a
AayushiPastagia <53651953+AayushiPastagia@users.noreply.github.com>
2021-05-24 15:47:31 +0530

    model file

---
+++ model.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv('hiring.csv')
+
+df['experience'].fillna(0, inplace = True)
+df['test_score'].fillna(df['test_score'].mean(), inplace=True)
+
+X = df.iloc[:, :3]
+y = df.iloc[:, -1]
+
+#Convert words to integer value
+def convert_to_int(word):
+    word_dict = {'one':1, 'two':2, 'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9,'ten':10,'eleven':11,'twelve':12,'zero':0, 0:0}
+    return word_dict[word]
+
+X['experience'] = X['experience'].apply(lambda x: convert_to_int(x))
+
+#fit model
+regressor = LinearRegression()
+regressor.fit(X, y)
+
+#save model
+pickle.dump(regressor, open('model.pkl','wb'))
+
+#load model
+model = pickle.load(open('model.pkl','rb'))
+
+print(model.predict([[2,9,6]]))
